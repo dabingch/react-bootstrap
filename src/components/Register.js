332e2b3f433e405c8b87c2af9779aa880a3bbed5
Hao Chen <hche8093@uni.sydney.edu.au>
2022-10-20 21:09:50 +1100

    Form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,36 @@
-import React from "react";
-import { Row, Container, Col, Card, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Container, Col, Card, Form, Button } from "react-bootstrap";
 
 const Register = () => {
+	const [state, setState] = useState({
+		user: {
+			username: "",
+			email: "",
+			password: "",
+		},
+	});
+
+	const updateInput = (e) => {
+		setState({
+			...state,
+			user: {
+				...state.user,
+				[e.target.name]: e.target.value,
+			},
+		});
+	};
+
+	const register = (e) => {
+		e.preventDefault();
+		console.log(state.user);
+	};
+
 	return (
 		<div>
+			{/* <pre>{JSON.stringify(state)}</pre> */}
 			<Container className="mt-3">
 				<Row>
-					<Col xs={3}>
+					<Col md={3}>
 						<Card className="shadow-lg">
 							<Card.Header
 								className="p-3"
@@ -14,14 +38,39 @@ const Register = () => {
 							>
 								<h4>Register</h4>
 							</Card.Header>
-							<Card.Body>
+							<Card.Body style={{ backgroundColor: "#33333" }}>
 								<Form>
-									<Form.Group>
+									<Form.Group className="mb-3">
 										<Form.Control
+											name="username"
+											onChange={updateInput}
 											type="text"
-											placeholder="user name"
+											placeholder="Username"
 										/>
 									</Form.Group>
+									<Form.Group className="mb-3">
+										<Form.Control
+											name="email"
+											onChange={updateInput}
+											type="email"
+											placeholder="Email"
+										/>
+									</Form.Group>
+									<Form.Group className="mb-3">
+										<Form.Control
+											name="password"
+											onChange={updateInput}
+											type="password"
+											placeholder="Password"
+										/>
+									</Form.Group>
+									<Button
+										onClick={register}
+										type="submit"
+										variant="success"
+									>
+										Submit
+									</Button>
 								</Form>
 							</Card.Body>
 						</Card>
